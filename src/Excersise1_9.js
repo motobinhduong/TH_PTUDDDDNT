9a389c424ea8479957b437ea998082d3e894370a
motobinhduong <lamphamtayninh@gmail.com>
2024-04-12 08:55:12 +0700

    capnhat

--- src/Excersise1_9.js
+++ src/Excersise1_9.js
@@ -5,26 +5,81 @@ import {Entypo} from '@expo/vector-icons'
 
 
 export default function App(){
+  const style = StyleSheet.create({
+    result:{
+      backgroundColor: darkMode ? '#282f3b' : '#f5f5f5',
+      maxWidth:'100%',
+      maxHeight:'100%',
+      alignItems:'flex-end',
+      justifyContent:'flex-end',
+  
+    },
+    resultText:{
+      maxHeight:20,
+      color:'#00b9d6',
+      margin:15,
+      fontSize:20,
+    },
+    historyText:{
+      color : darkMode ? '#B5B7BB' : '@7c7c7c',
+      fontSize:20,
+      marginRight:20,
+      alignSelf:'flex-end',
+  
+    },
+    themeButton:{
+      alignSelf:'flex-start',
+      bottom:'5%',
+      margin:15,
+      backgroundColor: darkMode ? '#7b8040' : '#e5e5e5',
+      alignItems: 'center',
+      justifyContent:'center',
+      width:50,
+      height:50,
+      borderRadius:25,
+  
+    },
+    buttons:{
+      width:'100%',
+      height:'35%',
+      flexDirection:'row',
+      flexWrap: 'wrap'
+    },
+    button:{
+      borderColor:darkMode ? '#3f4d5b' : '#e5e5e5',
+      alignItems:'center',
+      justifyContent:'center',
+      minWidth:'22%',
+      minHeight:'30%',
+      flex:1,
+      borderRadius:10,
+  
+    },
+    textButton:{
+      color: darkMode ? '#b5b7bb' : '#7c7c7c',
+      fontSize:20,
+    }
+  })
   const [darkMode, setDarkmode] =useState(false);
-  const [currentNumber, setcurrentNumber] = useState('');
+  const [currentNumber, setCurrentNumber] = useState('');
   const [lastNumber, setLastNumber] = useState('');
   const buttons =['C', 'DEL', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']
   function calculator(){
     let lastArr = currentNumber[currentNumber.length-1];
     if(lastArr === '/' || lastArr === '*' || lastArr === '-' || lastArr === '+' || lastArr === '.'){
-      setcurrentNumber(currentNumber)
+      setCurrentNumber(currentNumber)
       return
     }
     else{
       let result = eval(currentNumber).toString();
-      setcurrentNumber(result)
+      setCurrentNumber(result)
       return
     }
   }
   function handleInput(buttonPressed){
     if(buttonPressed === '+' || buttonPressed === '-' || buttonPressed === '*' || buttonPressed ==='/'){
       Vibration.vibrate(35);
-      setcurrentNumber(currentNumber + buttonPressed)
+      setCurrentNumber(currentNumber + buttonPressed)
       return
     }
     else if(buttonPressed === 1 || buttonPressed ===2 || buttonPressed ===3 ||buttonPressed === 4
@@ -35,12 +90,12 @@ export default function App(){
       switch(buttonPressed){
         case 'DEL':
           Vibration.vibrate(35);
-          setcurrentNumber(currentNumber.substring(0, (currentNumber.length -1)))
+          setCurrentNumber(currentNumber.substring(0, (currentNumber.length -1)))
           return
         case 'C':
           Vibration.vibrate(35);
           setLastNumber('')
-          setcurrentNumber('')
+          setCurrentNumber('')
           return
         case '=':
           Vibration.vibrate(35);
@@ -48,7 +103,7 @@ export default function App(){
           calculator()
           return
       }
-      setcurrentNumber(currentNumber + buttonPressed)
+      setCurrentNumber(currentNumber + buttonPressed)
       
   }
   return(
@@ -61,7 +116,7 @@ export default function App(){
         <Text style={style.resultText}>{currentNumber}</Text>
       </View>
       <View style={style.buttons}>
-        {buttons.map((buttons) =>
+        {buttons.map((button) =>
       button === '=' || button === '/' || button === '*' || button === '-' || button === '+' ?
     <TouchableOpacity key={button} style={[style.button, {backgroundColor: '#00b9d6'}]} onPress={() => handleInput(button)}>
       <Text style={[style.textButton, {color:'whtie', fontSize:28 }]}>{button}</Text>
@@ -74,6 +129,13 @@ export default function App(){
     <Text style={style.textButton}>{button}</Text>
   </TouchableOpacity>
   :
+  button === 'C' ?
+  <TouchableOpacity key={button}style={[style.button,{backgroundColor:typeof(button) === 'number'?
+  darkMode ? '#303946':'fff': darkMode === true ? '#414853' : '#ededed', minWidth: '36%'}]}
+  onPress={()=> handleInput(button)}>
+    <Text style={style.textButton}>{button}</Text>
+    </TouchableOpacity>
+  :
   <TouchableOpacity key={button} style={[style.button, {backgroundColor:typeof(button) === 'number'?
   darkMode ? '#303946' : 'fff' : darkMode === true ? '#414853' : '#ededed' }]} onPress={() => handleInput(button)}>
     <Text style={ style.textButton}>{button}</Text>
@@ -82,57 +144,5 @@ export default function App(){
      </View>
     </View>
   )
-const style = StyleSheet.create({
-  result:{
-    backgroundColor: darkMode ? '#282f3b' : '#f5f5f5',
-    maxWidth:'100%',
-    maxHeight:'35%',
-    alignItems:'flex-end',
-    justifyContent:'flex-end',
-
-  },
-  resultText:{
-    maxHeight:45,
-    color:'#00b9d6',
-    margin:15,
-    fontSize:35,
-  },
-  historyText:{
-    color : darkMode ? '#B5B7BB' : '@7c7c7c',
-    fontSize:20,
-    marginRight:10,
-    alignSelf:'flex-end',
-
-  },
-  themeButton:{
-    alignSelf:'flex-start',
-    bottom:'5%',
-    margin:15,
-    backgroundColor: darkMode ? '#7b8040' : '#e5e5e5',
-    alignItems: 'center',
-    justifyContent:'center',
-    width:50,
-    height:50,
-    borderRadius:25,
-
-  },
-  buttons:{
-    width:'100%',
-    height:'35%',
-    flexDirection:'row',
-    flexWrap: 'wrap'
-  },
-  button:{
-    borderColor:darkMode ? '#3f4d5b' : '#e5e5e5',
-    alignItems:'center',
-    justifyContent:'center',
-    minWidth:'24%',
-    minHeight:'54%',
-    flex:2
-  },
-  textButton:{
-    color: darkMode ? '#b5b7bb' : '#7c7c7c',
-    fontSize:28,
-  }
-})
+  
 }
